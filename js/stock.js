1fb4e4b9016da93255bb46d3c04a8c11f7f607f7
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-10-31 14:25:32 -0300

    change footer

---
+++ js/stock.js
@@ -0,0 +1,10 @@
+
+let stockProductos = [
+    {   
+        id:1, 
+        nombre:"Colombian coffee", 
+        precio: 15, 
+        img: './img/granoCafe.jpg', 
+        cantidad: 1
+    }
+]
---
+++ script.js
@@ -0,0 +1,33 @@
+const productsContainer = document.getElementById('product1');
+
+//Show products 
+showProducts(stockProductos)
+
+function showProducts(array) {
+    productsContainer.innerHTML = '';
+    for (const producto of array) {
+        let div = document.createElement('div')
+        div.className = 'section-p1'
+        div.innerHTML += `<h2>Featured products</h2>
+                            <p>Summer Collection New Morden Design</p>
+                            <div class="pro-container">
+                                <div class="pro">
+                                    <img class="img-prod" src=${producto.img}>
+                                    <div class="des">
+                                        <span>Colombian Coffee</span>
+                                        <h5>Grane Colombian Coffee  </h5>
+                                        <div class="star">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </div>
+                                        <h4>$15</h4>
+                                    </div>
+                                    <a href="#"><i class="fa-solid fa-cart-arrow-down"></i></a>
+                                </div>
+                            </div>`
+        productsContainer.appendChild(div);
+    }
+}

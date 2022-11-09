489baa9852057405b9963cd43ef8281f879fca2d
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-11-09 14:14:28 -0300

    media

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 const productsContainer = document.getElementById('product1');
+
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
 
 //Show products 
 showProducts(stockProductos)

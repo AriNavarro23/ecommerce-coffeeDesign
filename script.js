077a138dcdae04d06ac2e49c45b294c98fcc6b24
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-11-20 20:03:13 -0300

    sproduct

--- script.js
+++ script.js
@@ -20,20 +20,20 @@ if ( close ) {
 
 /* show small products */
 
-const MainImg = document.getElementById('MainImg');
-const smallimg = document.getElementsByClassName('small-img');
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
 
-smalling[0].onclick = function(){
-    MainImg.src = smalling[0].src;
+smallimg[0].onclick = function() {
+    MainImg.src = smallimg[0].src;
 }
-smalling[1].onclick = function(){
-    MainImg.src = smalling[1].src;
+smallimg[1].onclick = function() {
+    MainImg.src = smallimg[1].src;
 }
-smalling[2].onclick = function(){
-    MainImg.src = smalling[2].src;
+smallimg[2].onclick = function() {
+    MainImg.src = smallimg[2].src;
 }
-smalling[3].onclick = function(){
-    MainImg.src = smalling[3].src;
+smallimg[3].onclick = function() {
+    MainImg.src = smallimg[3].src;
 }
 
 //Show products 
